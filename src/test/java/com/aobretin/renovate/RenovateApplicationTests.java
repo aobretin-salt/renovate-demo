d1993f0e64a5695108dc4353c3f18b508a15a28d
Alexandru Obretin <alexandru.obretin@salt.bank>
2026-07-31 15:16:40 +0300

    intiial

---
+++ src/test/java/com/aobretin/renovate/RenovateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aobretin.renovate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenovateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
